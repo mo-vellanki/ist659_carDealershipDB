6c1bf4a59c9519287abd926483661e7ebe2b2351
Mo <mohit-vellanki@users.noreply.github.com>
2022-12-03 13:56:17 -0500

    [6] done(ish) with table creation and inserts
    TODO
    -need to fix FK fuck up
    -create trigger, stored proc etc

--- carDealershipDB.sql
+++ carDealershipDB.sql
@@ -8,6 +8,26 @@ IF EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS
     ALTER TABLE cars DROP CONSTRAINT FK_cars_car_type
 GO
 IF EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS
+    WHERE CONSTRAINT_NAME='FK_cars_car_condition')
+    ALTER TABLE cars DROP CONSTRAINT FK_cars_car_condition
+GO
+IF EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS
+    WHERE CONSTRAINT_NAME='FK_cars_car_transmission')
+    ALTER TABLE cars DROP CONSTRAINT FK_cars_car_transmission
+GO
+IF EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS
+    WHERE CONSTRAINT_NAME='FK_cars_car_seller_user_id')
+    ALTER TABLE cars DROP CONSTRAINT FK_cars_car_seller_user_id
+GO
+IF EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS
+    WHERE CONSTRAINT_NAME='FK_cars_car_buyer_user_id')
+    ALTER TABLE cars DROP CONSTRAINT FK_cars_car_buyer_user_id
+GO
+IF EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS
+    WHERE CONSTRAINT_NAME='FK_cars_car_buyer_user_id')
+    ALTER TABLE cars DROP CONSTRAINT FK_cars_car_buyer_user_id
+GO
+IF EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS
     WHERE CONSTRAINT_NAME='FK_cars_info_car_id')
     ALTER TABLE cars_information DROP CONSTRAINT FK_cars_info_car_id
 GO
@@ -26,7 +46,24 @@ GO
 IF EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS
     WHERE CONSTRAINT_NAME='FK_users_score_user_id')
     ALTER TABLE users_score_lookup DROP CONSTRAINT FK_users_score_user_id
+GO
+IF EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS
+    WHERE CONSTRAINT_NAME='FK_preference_user_id')
+    ALTER TABLE users_preference DROP CONSTRAINT FK_preference_user_id
+GO
+IF EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS
+    WHERE CONSTRAINT_NAME='FK_CAR_RATINGS_FOR_CAR_ID')
+    ALTER TABLE car_ratings DROP CONSTRAINT FK_CAR_RATINGS_FOR_CAR_ID
+GO
+IF EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS
+    WHERE CONSTRAINT_NAME='FK_users_user_credit_score_id')
+    ALTER TABLE users DROP CONSTRAINT FK_users_user_credit_score_id
 PRINT('Dropped existing constraints')
+
+-- Drop the views if they already exist
+GO
+DROP VIEW IF EXISTS v_cars_avail_basedon_user_preference
+PRINT('Dropped existing views')
 
 -- Drop the tables if they already exist
 GO
@@ -89,13 +126,11 @@ CREATE TABLE users(
     ,[user_address_state] CHAR(2) NOT NULL
     ,[user_phonenumber_areacode] SMALLINT NOT NULL -- Takes 6bytes to store a split phone number rather than 8bytes for storing it as a single number
     ,[user_phonenumber_telephone] INT NOT NULL
-    ,[user_credit_score] SMALLINT NOT NULL
+    ,[user_credit_score_id] SMALLINT NOT NULL
     ,CONSTRAINT [PK_users_user_id] PRIMARY KEY (user_id)
+    ,CONSTRAINT [FK_users_user_credit_score_id] FOREIGN KEY (user_credit_score_id) REFERENCES users_score_lookup(users_score_user_id)
     ,CONSTRAINT [UC_users_user_email] UNIQUE (user_email)
-    ,CONSTRAINT [CC_user_credit_score_range] CHECK (user_credit_score >=300 AND user_credit_score<=850)
 )
-
-
 
 GO
 -- Create the cartype_lookup table used in cars table
@@ -128,6 +163,7 @@ CREATE TABLE cars(
     ,[car_type] NVARCHAR(10) NOT NULL
     ,[car_available] BIT NOT NULL CONSTRAINT DV_cars_car_available DEFAULT 1 -- 0=False, 1=True if car is available or not
     ,[car_asking_price] SMALLMONEY NOT NULL -- Assuming we're only dealing cars that cost <214,748
+    ,[car_sell_by] DATETIME NOT NULL CONSTRAINT DV_cars_car_sell_by DEFAULT (DATEADD(day, 30, GETDATE())) -- Bidding period for each car is 30days
     ,[car_seller_user_id] TINYINT NOT NULL
     ,[car_buyer_user_id] TINYINT NULL
     ,[car_amount_sold] SMALLMONEY NULL -- Assuming we're only dealing cars that cost <214,748
@@ -167,9 +203,10 @@ CREATE TABLE cars_information(
 -- Create the bid_status_lookup table that contains possible values for bid_status column in the bids table
 GO -- Create bid_status_lookup table
 CREATE TABLE bid_status_lookup(
-     [bid_status_id] BIT NOT NULL PRIMARY KEY
+     [bid_status_id] BIT NOT NULL
     ,[bid_status_status] CHAR(3) NOT NULL
-
+    ,CONSTRAINT [PK_bid_status_lookup] PRIMARY KEY (bid_status_id)
+    ,CONSTRAINT [U_bid_status_status] UNIQUE (bid_status_status)
 )
 
 -- Create the bids table that contains information about bids placed on cars
@@ -184,26 +221,28 @@ CREATE TABLE bids(
     ,CONSTRAINT [PK_bids_bid_id] PRIMARY KEY (bid_id)
     ,CONSTRAINT [FK_bids_bid_user_id] FOREIGN KEY (bid_user_id) REFERENCES users(user_id)
     ,CONSTRAINT [FK_bids_bid_car_id] FOREIGN KEY (bid_car_id) REFERENCES cars(car_id)
-    -- ,CONSTRAINT [DF_bids_bid_date_time_current] DEFAULT bid_datetime (getdate())
-    ,CONSTRAINT [CC_bids_bid_status] FOREIGN KEY (bid_status) REFERENCES bid_status_lookup(bid_status_id)
+    ,CONSTRAINT [FK_bids_bid_status] FOREIGN KEY (bid_status) REFERENCES bid_status_lookup(bid_status_id)
 )
 
 -- Create users_score_lookup
 GO -- Create the table
 CREATE TABLE score_provider_lookup(
-    [provider_id] TINYINT NOT NULL PRIMARY KEY
+    [provider_id] TINYINT NOT NULL IDENTITY(1,1)
     ,[provider_name] NVARCHAR(30) NOT NULL
+    ,CONSTRAINT [PK_score_provider_lookup_provider_id] PRIMARY KEY (provider_id)
 )
 -- Create users_score_lookup
 GO -- Create the table
 CREATE TABLE users_score_lookup(
-    [users_score_id] TINYINT NOT NULL
-    ,[users_score_user_id] TINYINT NOT NULL
-    ,[users_score_user score] SMALLINT NOT NULL
+    [users_score_id] TINYINT NOT NULL IDENTITY(1,1)
+    [users_score_user_id] TINYINT NOT NULL
+    ,[users_score_credit_score] SMALLINT NOT NULL
     ,[users_score_provider_id] TINYINT NOT NULL
     ,CONSTRAINT [PK_users_score_id] PRIMARY KEY (users_score_id)
     ,CONSTRAINT [FK_users_score_user_id] FOREIGN KEY (users_score_user_id) REFERENCES users(user_id)
     ,CONSTRAINT [FK_users_score_provider_id] FOREIGN KEY (users_score_provider_id) REFERENCES score_provider_lookup(provider_id)
+    ,CONSTRAINT [CC_user_credit_score_range] CHECK (users_score_credit_score >=300 AND users_score_credit_score<=850)
+
 )
 -- Create user_preferences
 GO -- Create the table
@@ -219,38 +258,100 @@ CREATE TABLE users_preference(
 -- Create car_ratings
 GO -- Create the table
 CREATE TABLE car_ratings(
-    [rating_id] TINYINT NOT NULL
+    [rating_id] TINYINT NOT NULL IDENTITY(1,1)
     ,[rating_for_car_id] TINYINT NOT NULL
     ,[rating_value] TINYINT NOT NULL
     ,[rating_comments] NVARCHAR NOT NULL
     ,CONSTRAINT [PK_CAR_RATINGS_RATING_ID] PRIMARY KEY (rating_id)
     ,CONSTRAINT [FK_CAR_RATINGS_FOR_CAR_ID] FOREIGN KEY (rating_for_car_id) REFERENCES cars(car_id)
 )
-
+GO
 PRINT('.....Tables created')
 
 -- Inserting values into tables
 GO
 PRINT('Inserting Data into tables')
 GO
+INSERT INTO users
+    (user_email, user_firstname, user_lastname, user_address_street, user_address_city
+    , user_address_state,user_phonenumber_areacode, user_phonenumber_telephone, user_credit_score_id)
+    VALUES
+    ('roy.walker@hotmail.com', 'Roy', 'Walker', '45 Sunset St.', 'East Lafayette', 'IN',134,456745,1)
+    ,('nate@gmail.com', 'Nate', 'Turner', '50 Longfellow St.', 'Harbor Township', 'NJ',235,567890,680)
+    ,('joy.alan@hotmail.com', 'Joy', 'Alan', '50 Sunset St.', 'East Lafayette', 'IN',134,496745,756)
+    ,('grace.smith@hotmail.com', 'Grace', 'Smith', '67 Sunset St.', 'East Lafayette', 'IN',134,458945,766)
+
+GO
 INSERT INTO cartypes_lookup(cartype_type)
     VALUES ('Sedan'),('Coupe'),('Hatchback'),('SUV'),('Van')
 GO
 INSERT INTO car_conditions_lookup(car_conditions_condition)
-    VALUES ('Bad'), ('Average'), ('Good'), ('Very Good'), ('Excellent') 
+    VALUES ('Bad'), ('Average'), ('Good'), ('Very Good'), ('Excellent')
 GO
 INSERT INTO car_transmissions_lookup
     VALUES ('A'), ('M')
 GO
-INSERT INTO users
-    (user_email, user_firstname, user_lastname, user_address_street, user_address_city
-    , user_address_state,user_phonenumber_areacode, user_phonenumber_telephone, user_credit_score)
-    VALUES
-    ('rocio.walker@hotmail.com', 'Rocio', 'Walker', '46 Sunset St.', 'West Lafayette', 'IN',123,456789,769),
-    ('nikita1@gmail.com', 'Nikita', 'One', '30 Longfellow St.', 'Egg Harbor Township', 'NJ',234,567890,680)
-GO
 INSERT INTO cars(car_name, car_type, car_asking_price, car_seller_user_id)
     VALUES ('Volvo XC60', 'SUV', 65000,1)
+INSERT INTO cars(car_name, car_type, car_asking_price, car_seller_user_id)
+    VALUES ('Hyndai IONIQ', 'Sedan', 50000,2)
+INSERT INTO cars(car_name, car_type, car_asking_price, car_seller_user_id)
+    VALUES ('Nissan Rogue Hybrid 2019', 'SUV', 65000,2)
+INSERT INTO cars(car_name, car_type, car_asking_price, car_seller_user_id)
+    VALUES ('Toyoto GR86', 'Coupe', 55000,1)
+INSERT INTO cars(car_name, car_type, car_asking_price, car_seller_user_id)
+    VALUES ('Subaru Crosstrek', 'Hatchback', 45000,1)
+INSERT INTO cars(car_name, car_type, car_asking_price, car_seller_user_id)
+    VALUES ('Volvo XC60', 'SUV', 65000,1)
+INSERT INTO cars(car_name, car_type, car_asking_price, car_seller_user_id)
+    VALUES ('Honda Sienna', 'Van', 65000,1)
+GO
+INSERT INTO cars_information
+    (cars_info_car_id,cars_info_car_description,cars_info_car_transmission,cars_info_car_colour,
+    cars_info_car_yearOfManf,cars_info_car_fueltype,cars_info_car_mileage_000,cars_info_car_noof_prev_owners,cars_info_car_condition)
+    VALUES
+    (100,'Intelligent design at every turn. Meet our smart midsize SUV with Google built-in','A','Red',2020,'ELE',4,2,5)
+    ,(101,'Discover inner harmony. Contemporary design and more sustainable materials characterize every detail','A','Red',2020,'ELE',31,2,5)
+    ,(102,'Good Condition','A','black',2001,'GAS',3,2,4)
+    ,(103,'Spacious','M','Blue',2011,'HYB',1,2,5)
+    ,(104,'Inner Harmony' ,'A','yellow',2013,'GAS',11,8,5)
+    ,(105,'Contemporary design and more sustainable materials characterize every detail','A','Red',2020,'ELE',13,7,5)
+    ,(106,'inner harmony. Contemporary design','M','green',2020,'HYB',24,8,4)
+    -- ,(107,'Discover inner harmony. Contemporary design more sustainable materials characterize every detail','M','Red',2020,'HYB',5,4,2)
+    -- ,(108,' sustainable materials characterize every detail','A','Blue',2020,'ELE',3,2,2)
+    -- ,(109,'Contemporary design','A','Black',2020,'GAS',31,3,4)
+GO
+INSERT INTO bid_status_lookup VALUES (0, 'NOK') ,(1, 'OK')-- , (4,'OK')
 
 GO
-select * from INFORMATION_SCHEMA.Tables
+INSERT INTO bids (bid_user_id,bid_car_id,bid_amount,bid_status)
+    VALUES (1,100,100000,0)
+            ,(2,101,65000,1)
+            ,(3,102,70000,0)
+            ,(4,104,80000,1)
+GO
+INSERT INTO score_provider_lookup(provider_name) values ('Equifax'),('TransUnion'),('Experian')
+GO
+INSERT INTO users_score_lookup(users_score_user_id,users_score_credit_score,users_score_provider_id)
+    VALUES (1, 600, 2)
+            ,(2, 800, 1)
+            ,(3, 740, 3)
+            ,(4, 690, 2)
+
+GO
+INSERT INTO users_preference(preference_user_id,preference_max_price,preference_color,preference_fueltype,preference_transmission)
+    VALUES (3,28000,'Green','HYB','A')
+            ,(1,18000,'Red','HYB','A')
+            ,(2,28000,'Green','ELE','A')
+            ,(4,29000,'Green','HYB','A')
+            --,(5,28000,'Blue','HYB','A')
+
+GO
+INSERT INTO car_ratings(rating_id,rating_for_car_id,rating_value,rating_comments)
+    VALUES (1,100,4,'Mint condition')
+            ,(2,102,2,'Needs some fixes')
+            ,(3,103,3,'Runs fine with random noises')
+            ,(4,107,5,'excellent')
+
+GO
+PRINT('.....Data inserted')
